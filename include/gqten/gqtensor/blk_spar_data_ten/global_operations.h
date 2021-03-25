94d9d4451cf6e6dfabf1f9f174fc63bda6b638c8
Rong-Yang Sun <sun-rongyang@outlook.com>
2021-03-25 20:03:58 +0800

    fix(TensorExpansion): Not create data block to embed tensor B

--- include/gqten/gqtensor/blk_spar_data_ten/global_operations.h
+++ include/gqten/gqtensor/blk_spar_data_ten/global_operations.h
@@ -424,9 +424,11 @@ void BlockSparseDataTensor<ElemT, QNT>::ConstructExpandedDataFrom(
   auto qninfohash_blkcoors_shape_map = GenQnInfoHashBlkCoorsShapeMap(*this);
   auto blk_idx_data_blk_map_a = bsdt_a.GetBlkIdxDataBlkMap();
   std::vector<size_t> tensor_embed_offsets_a(ten_rank, 0);
+  std::vector<size_t> bsdt_a_existed_blk_qnhashs;
   for (auto &blk_idx_data_blk : blk_idx_data_blk_map_a) {
     auto data_blk_a = blk_idx_data_blk.second;
     auto qnhash = data_blk_a.GetQNBlkInfo().QnHash();
+    bsdt_a_existed_blk_qnhashs.push_back(qnhash);
     assert(
         qninfohash_blkcoors_shape_map.find(qnhash) !=
         qninfohash_blkcoors_shape_map.end()
@@ -450,14 +452,21 @@ void BlockSparseDataTensor<ElemT, QNT>::ConstructExpandedDataFrom(
         qninfohash_blkcoors_shape_map.end()
     );
     auto blk_coors_shape_pair = qninfohash_blkcoors_shape_map[qnhash];
-    if (data_blk_b.shape != blk_coors_shape_pair.second) {
-      std::vector<size_t> tensor_embed_offsets_b;
-      tensor_embed_offsets_b.reserve(ten_rank);
-      for (size_t i = 0; i < ten_rank; ++i) {
-        tensor_embed_offsets_b.push_back(
-            blk_coors_shape_pair.second[i] - data_blk_b.shape[i]
-        );
-      }
+    // Calculate embed offsets
+    std::vector<size_t> tensor_embed_offsets_b;
+    tensor_embed_offsets_b.reserve(ten_rank);
+    for (size_t i = 0; i < ten_rank; ++i) {
+      tensor_embed_offsets_b.push_back(
+          blk_coors_shape_pair.second[i] - data_blk_b.shape[i]
+      );
+    }
+    auto bsdt_a_blk_qnhash_it = std::find(
+                                    bsdt_a_existed_blk_qnhashs.begin(),
+                                    bsdt_a_existed_blk_qnhashs.end(),
+                                    qnhash
+                                );
+    // If the data block is existed
+    if (bsdt_a_blk_qnhash_it != bsdt_a_existed_blk_qnhashs.end()) {
       auto data_blk_expanded = GetBlkIdxDataBlkMap().at(
                                    BlkCoorsToBlkIdx(blk_coors_shape_pair.first)
                                );
@@ -470,7 +479,6 @@ void BlockSparseDataTensor<ElemT, QNT>::ConstructExpandedDataFrom(
     } else {
       auto blk_idx_data_blk_it = DataBlkInsert(blk_coors_shape_pair.first);
       auto data_blk_expanded = blk_idx_data_blk_it->second;
-      std::vector<size_t> &tensor_embed_offsets_b = tensor_embed_offsets_a;
       RawDataEmbed_(
           bsdt_b.GetActualRawDataPtr(),
           data_blk_b,
